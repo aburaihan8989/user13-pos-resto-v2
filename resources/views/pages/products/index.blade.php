debf33d3b966b808946200a6c21b0682f37e0099
aburaihan8989 <yusup.smartphone@gmail.com>
2025-03-12 08:42:06 +0700

    update

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -135,7 +135,7 @@
                                                     <div class="d-flex">
                                                         @if ( auth()->user()->roles == "admin" )
                                                             <a href='{{ route('product.edit', $product->id) }}'
-                                                                class="btn btn-sm btn-info btn-icon">
+                                                                class="btn btn-sm btn-primary btn-icon">
                                                                 <i class="fas fa-edit"></i>
                                                                 Edit
                                                             </a>
@@ -153,7 +153,7 @@
                                                             <input type="hidden" name="_method" value="DELETE" />
                                                             <input type="hidden" name="_token"
                                                                 value="{{ csrf_token() }}" />
-                                                                <button class="btn btn-sm btn-info btn-icon confirm-delete ml-2">
+                                                                <button class="btn btn-sm btn-danger btn-icon confirm-delete ml-2">
                                                                 <i class="fas fa-times"></i> Delete
                                                             </button>
                                                         </form>
